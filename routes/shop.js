0b3d8823f8fa51de917422d441c7c657fa40a408
joytrinh <mongtuyen149@gmail.com>
2020-07-05 22:09:04 +0700

    Fix route bugs

--- routes/shop.js
+++ routes/shop.js
@@ -19,10 +19,6 @@ router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct)
 
 router.get('/checkout', isAuth, shopController.getCheckout);
 
-router.get('/checkout/success', shopController.getCheckoutSuccess);
-
-router.get('/checkout/cancel', shopController.getCheckout);
-
 router.get('/orders', isAuth, shopController.getOrders)
 
 // router.post('/create-order', isAuth, shopController.postOrder)
